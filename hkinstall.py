c920fc0dd4940bdb0b72e0d7866c5b6e7e360ef4
Mathieu Guigue <mgguigue@gmail.com>
2023-10-13 13:56:30 +0200

    Add post installation step
    Add sym link of the installed directory into the shared compiled tools directory

--- hkinstall.py
+++ hkinstall.py
@@ -6,3 +6,19 @@ class hkLibToolPackTemplate(CMake):
     def __init__(self, path):
         super().__init__(path)
         self._package_name = "hk-LibToolPackTemplate"
+
+    def post_install(self):
+        # Run standard post_install step
+        super().post_install()
+
+        # Link compiled tool into
+        compiled_tools_folder = os.environ.get("HK_COMPILEDTOOLS_DIR")
+        logger.info(f"Linking installation to {compiled_tools_folder}")
+        if not os.path.exists(compiled_tools_folder):
+            logger.debug("Folder doesn't exist: creating it")
+            os.mkdir(compiled_tools_folder)
+        symlink = os.path.join(compiled_tools_folder, self._package_name)
+        # if os.path.exists(symlink):
+        os.symlink(self._install_folder, symlink)
+        return True
+
